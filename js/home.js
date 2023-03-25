63af5d7f9dbb35cb37fd879400a2a2118a65c90a
MatheusAlves099 <mts02501@gmail.com>
2023-03-25 02:54:45 -0300

    design/responsividade finalizados

---
+++ js/home.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import { loadCard } from "./homeAPI"
+
+const cards = await loadCard;
+
+const createCard = (card) => {
+    const courseButtons = document.createElement('div')
+    courseButtons.classList.add('buttonsCourse')
+
+    const containerCourse = document.createElement('div')
+    containerCourse.classList.add('course-container')
+
+    const courseInfo = document.createElement('div')
+    courseInfo.classList.add('info-course')
+
+    const iconCourse = document.createElement('i')
+    iconCourse.src = `./${card.icone}`
+
+    const nameCourse = document.createElement('h1')
+    nameCourse.classList.add('course-text')
+    nameCourse.textContent = card.name
+
+    courseButtons.append(containerCourse)
+    containerCourse.append(courseInfo)
+    courseInfo.append(iconCourse, nameCourse)
+
+    return courseButtons
+}
+
+const loadCards = () => {
+    const listCourses = document.getElementById('buttonsCourse')
+    const coursesList = cards.map(createCard)
+
+    listCourses.replaceChildren(...coursesList)
+}
---
+++ js/homeAPI.js
@@ -0,0 +1,7 @@
+export const loadCard = async () => {
+    const url = `http://localhost:8080/v1/lion-school/cursos`
+    const response = await fetch(url)
+    const card = await response.json()
+
+    return card
+}
